bfb822000d79ffe2433d31f82470efccfd522e92
Brendan Collins <bcollins@blueraster.com>
2015-04-16 15:09:25 -0400

    hardcoded interpreter

--- arcpy_bridge/arcpy_bridge.py
+++ arcpy_bridge/arcpy_bridge.py
@@ -10,7 +10,11 @@ def run_model(toolbox_path, model_name,  model_args):
     arcpy.ImportToolbox(toolbox_path, toolbox_name)
     model_func = '{}_{}'.format(model_name, toolbox_name)
     print 'about to run model {}'.format(model_func)
-    return getattr(arcpy, model_func)(*model_args)
+
+    if model_args:
+        return getattr(arcpy, model_func)(*model_args)
+    else:
+        return getattr(arcpy, model_func)()
 
 def main():
     funcs = {}
