589734b95f785cfb3c22455a265d513ff7477454
Brendan Collins <bcollins@blueraster.com>
2015-04-16 13:30:52 -0400

    updated bridge path

--- arcpy_bridge/arcpy_process.py
+++ arcpy_bridge/arcpy_process.py
@@ -4,7 +4,9 @@ import subprocess
 def run_model(toolbox_path, model_name, model_args=[]):
     py = find_python_interpreter()
     print 'using python interpreter {}'.format(py)
-    bridge = os.path.join(os.path.abspath(__file__, 'arcpy_bridge.py'))
+    bridge = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'arcpy_bridge.py'))
+    print 'using bridge file {}'.format(bridge)
+
     bridge_args = ','.join([str(a) for a in model_args])
     proc = subprocess.Popen([py, bridge,  'run_model', toolbox_path, model_name, bridge_args], stdout=subprocess.PIPE, shell=True)
     return proc.communicate()
@@ -16,4 +18,5 @@ def find_python_interpreter():
                 for f in filenames:
                     if 'python27.exe' in f:
                         return os.path.join(dirname, f)
+
     raise Exception('Unable to find ArcGIS python interpreter')
