30b75ad12b56ea876c2aa52d433d9b9a882c7250
Brendan Collins <bcollins@blueraster.com>
2015-04-21 11:06:27 -0400

    added optional argument to set interpreter without manual discover

--- arcpy_bridge/arcpy_process.py
+++ arcpy_bridge/arcpy_process.py
@@ -1,12 +1,12 @@
 import os
 import subprocess
 
-def run_model(toolbox_path, model_name, model_args=None):
-    py = find_python_interpreter()
-    print 'using python interpreter {}'.format(py)
+def run_model(toolbox_path, model_name, model_args=None, interpreter=None):
+
+    if not interpreter:
+        py = find_python_interpreter()
+
     bridge = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'arcpy_bridge.py')
-    print 'using bridge file {}'.format(bridge)
-    print model_args    
     bridge_args = model_args and ','.join([str(a) for a in model_args]) or model_args
     proc = subprocess.Popen([py, bridge, 'run_model', toolbox_path, model_name, bridge_args], stdout=subprocess.PIPE, shell=True)
     results, err = proc.communicate()
