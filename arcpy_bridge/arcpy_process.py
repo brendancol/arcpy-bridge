29a58831717c248417e8bc60c91c17e09da3b9bf
Brendan Collins <bcollins@blueraster.com>
2015-04-21 15:25:37 -0400

    refactored python executable discovery

--- arcpy_bridge/arcpy_process.py
+++ arcpy_bridge/arcpy_process.py
@@ -2,10 +2,7 @@ import os
 import subprocess
 
 def run_model(toolbox_path, model_name, model_args=None, interpreter=None):
-
-    if not interpreter:
-        py = find_python_interpreter()
-
+    py = interpreter and interpreter or find_python_interpreter()
     bridge = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'arcpy_bridge.py')
     bridge_args = model_args and ','.join([str(a) for a in model_args]) or model_args
     proc = subprocess.Popen([py, bridge, 'run_model', toolbox_path, model_name, bridge_args], stdout=subprocess.PIPE, shell=True)
@@ -13,19 +10,11 @@ def run_model(toolbox_path, model_name, model_args=None, interpreter=None):
     return results, err
 
 def find_python_interpreter(x64=False):
-
-    if x64:
-        arcpy_folder_start = 'arcgisx6410.'
-    else:
-        arcpy_folder_start = 'arcgis10.'
-
+    arcpy_folder_start = x64 and 'arcgisx6410.' or 'arcgis10.' 
     for v in ['C:\\', 'D:\\']:
-        
         if not os.path.exists(v):
             continue
-
         for dirname, dirnames, filenames in os.walk(v):
             if 'Python27' in dirname and os.path.split(dirname)[1].lower().startswith(arcpy_folder_start):
                 return os.path.join(dirname, 'python.exe')
-
     raise Exception('Unable to find ArcGIS python interpreter')
